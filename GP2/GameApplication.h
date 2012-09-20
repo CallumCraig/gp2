97f42d80f73d1fbc602039a4f03086e5c2a387af
Callum Craig <ccraig27@caledonian.ac.uk>
2012-09-20 16:31:26 +0100

    Finished Lab 2 Bit has unhandled exeptions

--- GP2/GameApplication.h
+++ GP2/GameApplication.h
@@ -37,4 +37,25 @@ private:
 
 	ID3D10Effect*	m_pEffect; //holds our loaded effect file
 	ID3D10EffectTechnique*	m_pTechnique; //variable used in rendering process
+
+	ID3D10EffectMatrixVariable * m_pWorldMatrixVariable; //sends world matrix across effect
+
+	ID3D10EffectMatrixVariable * m_pViewMatrixVariable;
+	ID3D10EffectMatrixVariable * m_pProjectionMatrixVariable;
+
+	ID3D10EffectShaderResourceVariable * m_pDiffuseTextureVariable;
+	ID3D10ShaderResourceView * m_pTextureShaderResource;
+
+	D3DXMATRIX m_matView;
+	D3DXMATRIX m_matProjection;
+
+	D3DXMATRIX m_matWorld; //world matrix
+	D3DXMATRIX m_matScale; //world matrix scale
+	D3DXMATRIX m_matRotation; //world matrix rotation
+	D3DXMATRIX m_matTranslation; // world matrix translation
+
+	//used to represent the matrices as they are more human readable
+	D3DXVECTOR3 m_vecPosition; //vector used to represent the position
+	D3DXVECTOR3 m_vecRotation; //vector used to represent the rotation
+	D3DXVECTOR3 m_vecScale; //vector used to represent the scale
 };
